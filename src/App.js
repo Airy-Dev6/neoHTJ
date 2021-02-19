e25308fdfec8871606c4780f19af21c8820fbc9d
Airy-Dev6 <airysilva11@gmail.com>
2021-02-18 22:05:58 -0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { 
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar color="dark" light expand="md">
+          
+        <NavbarBrand className="text-white" href="/">Reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse color="white" isOpen={isOpen} navbar>
+        <ul className="navbar-nav">
+            <li className="nav-item ">
+             <Link className="nav-link text-white" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/about">Home</Link>
+            </li>
+            <li className="nav-item ">
+              <Link className="nav-link text-white" to="/">Home</Link>
+            </li>
+          </ul>
+        </Collapse>
+      </Navbar>
+      <Container>
+        <Switch>
+          <Route path="/about">
+            <h1>hola desde about</h1>
+          </Route>
+          <Route path="/users">
+            <h1>hola desde users</h1>
+          </Route>
+          <Route exact path="/">
+            <h1>hola desde home</h1>
+          </Route>
+        </Switch>
+        </Container>
+        </Router>
+    
+      
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Col, Row } from 'reactstrap'
+
+export default function index() {
+  return (
+    <div>
+      <Row>
+        <Col xs="3">
+        </Col>
+      </Row>
+    </div>
+  )
+}
